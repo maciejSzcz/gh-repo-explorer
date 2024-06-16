c3527e4245ee791e2bee7ddb1556242377aa45d8
Maciej Szczęsny <szczesnymcj@gmailc.om>
2024-06-16 19:18:42 +0200

    Removed faulty import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import Search from "views/Search";
 import Providers from "providers";
 import { GlobalStyle } from "./app.styled";
-import { queryClient } from "requests/queryClient";
 
 const App = () => {
   return (
-    <Providers queryClient={queryClient}>
+    <Providers>
       <GlobalStyle />
       <Search />
     </Providers>
